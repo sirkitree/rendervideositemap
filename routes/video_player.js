a1f110a1d37cd4f3f45bc7a1a69afcf15127ae75
David Burns <david@lullabot.com>
2016-11-12 12:15:40 -0500

    Adding placeholder text for title, description, and video browser

--- routes/video_player.js
+++ routes/video_player.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var draw = require("aframe-draw-component").component;
+var textwrap = require("aframe-textwrap-component").component;
+//AFRAME.registerComponent("draw", draw);
+//AFRAME.registerComponent("textwrap", textwrap);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('video_player', { title: 'Sitemap Video Player' });
