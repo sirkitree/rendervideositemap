614ab1c2ad6e32871d5cbdc874db1affe726043a
David Burns <david@lullabot.com>
2016-11-13 02:34:20 -0500

    Cleaning up code inside vr.js

--- public/javascripts/vr.js
+++ public/javascripts/vr.js
@@ -28,39 +28,54 @@ $(function() {
       // Make sure valid element was clicked
       if (vidId > -1) {
         var currentVid = $('#video-player').attr('src');
-        stopVideo(currentVid);
-        
-
         var newVid = $('#video-' + vidId);
-        // These are all the tags and attributes which need to be swapped.
-        var swap = {
-              video_player: {
-                attribute: 'src', prefix: '#video-' + vidId},
-              video_controller: {
-                attribute: 'video-controls', prefix: 'src:#video-' + vidId},
-              video_title: {
-                attribute: 'bmfont-text', prefix: 'text: ', data: 'title'},
-              video_desc: {
-                attribute: 'bmfont-text', prefix: 'text: ', data: 'description'},
-              video_pub: {
-                attribute: 'bmfont-text', prefix: 'text: Published on ', data: 'pubdate'},
-        };
-
-        // Loop through list and swap all the attributes.
-        $.each(swap, function(key, value) {
-          var selector = key.replace('_', '-');
-          var newVal = (value.data) ? newVid[0].dataset[value.data] : '';
-          d3.select('#' + selector).attr(value.attribute, value.prefix + newVal);
-        });
+        var elements = dynamicElements(vidId);
         
+        stopVideo(currentVid);
+        updateElements(elements, newVid);
         playVideo(newVid);
       }
     });
   });
+  
+  // These are all the tags and attributes which need to be swapped.
+  function dynamicElements(id) {
+    return {
+      video_player: {
+        attribute: 'src', 
+        prefix: '#video-' + id
+      },
+      // video_controller: {
+      //   attribute: 'video-controls', prefix: 'src:#video-' + vidId},
+      video_title: {
+        attribute: 'bmfont-text', 
+        prefix: 'text: ', 
+        data: 'title'
+      },
+      video_desc: {
+        attribute: 'bmfont-text', 
+        prefix: 'text: ', 
+        data: 'description'
+      },
+      video_pub: {
+        attribute: 'bmfont-text', 
+        prefix: 'text: Published on ', 
+        data: 'pubdate'
+      },
+    };
+  }
+
+  function updateElements(elements, video) {
+    // Loop through list and swap all the attributes.
+    $.each(elements, function(key, value) {
+      var selector = key.replace('_', '-');
+      var newVal = (value.data) ? video[0].dataset[value.data] : '';
+      d3.select('#' + selector).attr(value.attribute, value.prefix + newVal);
+    });
+  }
 
   function playVideo(selector) {
-    console.log($('#video-controller'));
-    $('#video-controller').attr('video-controls', 'src:' + selector);
+    //$('#video-controller').attr('video-controls', 'src:' + selector);
     $(selector).each(function () {
       this.currentTime = 0; 
       this.play() 
